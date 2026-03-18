c42ee394de5dcc7a01c006e38ecd4ddf33183013
Akanbi Omolade <omoladegraham@gmail.com>
2026-03-18 12:27:27 +0100

    highlights section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Highlights = () => {
-  return <div></div>;
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc-800"
+    >
+      <div className="screen-max-width">
+        <div>
+          <h1 id="title" className="section-heading">
+            Get the higlights.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Highlights;
