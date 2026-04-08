3887ddf1aaaa26aa12d9d85c34989ce55feb721d
Akanbi Omolade <omoladegraham@gmail.com>
2026-04-08 11:13:53 +0100

    video caroousel section

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,21 +1,96 @@
 import React from "react";
+
+import { useRef, useState } from "react";
 import { highlightsSlides } from "../constants";
+import { useEffect } from "react";
 
 const VideoCarousel = () => {
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
+
+  const [loadedData, setLoadedData] = useState([]);
+
+  const { isEnd, isLastVideo, startPlay, videoId, isPlaying } = video;
+
+  useEffect(() => {
+    if (loadedData.length > 3) {
+      if (!isPlaying) {
+        videoRef.current[videoId].pause();
+      } else {
+        startPlay && videoRef.current[videoId].play();
+      }
+    }
+  }, [startPlay, videoId, isPlaying, loadedData]);
+
+  useEffect(() => {
+    const currentProgress = 0;
+    let span = videoSpanRef.current;
+
+    if (span[videoId]) {
+      // animate the progress of the video
+      let anim = gsap.to(span[videoId], {
+        onUpdate: () => {},
+
+        onComplete: () => {},
+      });
+    }
+  }, [videoId, startPlay]);
+
   return (
-    <div className="flex items-center">
-      {highlightsSlides.map((list, i) => (
-        <div key={list.id} className="sm:pr-20 pr-10">
-          <div className="video-carousel_container">
-            <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-              <video muted playsInline preload="auto">
-                <source src={list.video} type="video/mp4" />
-              </video>
+    <>
+      <div className="flex items-center">
+        {highlightsSlides.map((list, i) => (
+          <div key={list.id} className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video
+                  muted
+                  playsInline
+                  preload="auto"
+                  ref={(el) => (videoRef.current[i] = el)}
+                  onPlay={() => {
+                    setVideo((prevVideo) => ({
+                      ...prevVideo,
+                      isPlaying: true,
+                    }));
+                  }}
+                >
+                  <source src={list.video} type="video/mp4" />
+                </video>
+              </div>
+              <div className="absolute top-12 left-[5%] z-10">
+                {list.textLists.map((text) => (
+                  <p key={text} className="md:text-2xl text-xl font-medium">
+                    {text}
+                  </p>
+                ))}
+              </div>
             </div>
           </div>
+        ))}
+      </div>
+
+      <div className="relative flex-center mt-10">
+        <div className="flex-center py-5 px-17 bg-gray-600 backdrop-blur rounded-full">
+          {videoRef.current.map((_, i) => {
+            <span
+              key={i}
+              ref={(el) => (videoDivRef.current[i] = el)}
+              className="mx-2 w-3 h-3 bg-gray-200"
+            ></span>;
+          })}
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 };
 
